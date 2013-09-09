65f433d525101f9ea2d42c214dfea98962ca4045
Riccardo Tacconi <Riccardo.Tacconi@wonga.com>
2013-09-09 15:36:01 +0100

    fixed spec

--- pantry_import_aws_bill_command/pantry_import_aws_bill_command.rb
+++ pantry_import_aws_bill_command/pantry_import_aws_bill_command.rb
@@ -4,10 +4,17 @@ module Wonga
       def initialize(api_client, config)
         @api_client = api_client
         @config = config
+        
+        # check if there are hidden messages
+        
+        # if there are hidden messages wait for the next
+        
+        # otherwise create the first message to init the loop
+        
       end
 
       def handle_message(message)
-        #Put message handling code in here
+        p message.inspect
       end
     end
   end
