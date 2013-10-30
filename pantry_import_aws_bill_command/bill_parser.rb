1e4c26f54b50800cf2a7c1a717a929be781dab87
Riccardo Tacconi <Riccardo.Tacconi@wonga.com>
2013-10-30 17:53:01 +0000

    TD-1332

--- pantry_import_aws_bill_command/bill_parser.rb
+++ pantry_import_aws_bill_command/bill_parser.rb
@@ -1,16 +1,38 @@
 require 'csv'
+require 'bigdecimal'
 
 module Wonga
   class BillParser
     def parse(text)
       result = {}
-      CSV.parse(text, headers: true) do |row|
+      result[:ec2] = []
+      
+      CSV.parse(text[/InvoiceID.*/], headers: true) do |row|
         if row["RecordType"] == "InvoiceTotal"
           result[:total_cost] = row["TotalCost"]
           result[:bill_date] = Time.parse(row["BillingPeriodEndDate"])
         end
+
+        if row["user:pantry_request_id"]
+          result[:ec2] << {
+            instance_id: row["user:pantry_request_id"],
+            cost: BigDecimal.new(row["TotalCost"]),
+            estimated: (row["InvoiceID"].strip == "Estimated")
+          }
+        end
       end
+      
+      grouped_costs = result[:ec2].group_by {|grouped_hash| grouped_hash[:instance_id]}
+      result[:ec2_total] = grouped_costs.map {|key, value| {estimated: reduce_estimated(value), instance_id: key, cost: BigDecimal.new(reduce_cost(value), 2)}}
       result
+    end
+    
+    def reduce_cost(value)
+      value.map {|item| item[:cost]}.reduce(:+)
+    end
+    
+    def reduce_estimated(value)
+      value.map {|item| item[:estimated]}.first
     end
   end
 end
